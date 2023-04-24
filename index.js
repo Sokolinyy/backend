6707e2f29750c4bcafef4e2064365b3b6ca9fc4e
Sokolinyy <kostyk.vasilii@gmail.com>
2023-04-24 19:46:48 +0200

    Deploy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import blogRoutes from "./routes/blogs.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 dotenv.config();
 
@@ -25,6 +26,6 @@ app.use("/create-blog", blogRoutes);
 
 // ...
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port 5000`);
 });
